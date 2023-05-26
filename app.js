db6da35b69469ccb03f19536fb4626d84ae6b2da
kanishka0325 <purna@web3genes.com>
2023-05-27 01:49:23 +0530

    test pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.json({msg: 'Hello, Backend!'});
+    res.json({msg: 'Hello, Backend CI/CD Pipeline!'});
 });
 
 app.listen(4000);
