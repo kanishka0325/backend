a9a105b576c80170f27daec5f7fb65f72cfd0aaa
Kanishka@Web3 Genes <133314889+kanishka0325@users.noreply.github.com>
2023-05-28 13:00:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get('/', (req, res)=>{
     res.json({msg: 'Hello, Backend CI/CD Pipeline!'});
 });
 
+app.get('/tets', (req, res)=>{
+    res.json({msg: 'Hello, Backend Test CI/CD pipeline!'});
+});
+
 app.listen(4000);
